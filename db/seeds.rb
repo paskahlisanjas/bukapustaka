2668ae707359d1bd70511f39dc62b05f1e62b62b
Paskahlis Anjas <pjaswo1204@gmail.com>
2018-05-24 15:38:32 +0700

    add new db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 Book.create({
-  :id => 1,
-  :title => 'A Brief History of Time',
-  :synopsis => 'A Brif History of Time tells us about how the time was created by the law of the universe at the first time',
-  :author => 'Stephen Hawking',
-  :image => File.new(File.join(Rails.root, 'dummy_cover', 'brief_history_of_time.jpg'))
-})  
+  :id => 2,
+  :title => 'By Invitation Only BAM Exclusive',
+  :synopsis => 'The Lowcountry of South Carolina is where By Invitation Only begins at a barbecue engagement party thrown by Diane English Stiftel, her brother Floyd, and her parents to celebrate her son’s engagement. On this gorgeous, magical night, the bride’s father, Alejandro Cambria, a wealthy power broker whose unbelievably successful career in private equity made him one of Chicago’s celebrated elite, discovers the limits and possibilities of cell phone range. While the mother of the bride, Susan Kennedy Cambria, who dabbles in the world of public relations and believes herself deserving of every square inch of her multimillion-dollar penthouse and imaginary carrara marble pedestal, learns about moonshine and dangerous liaisons. ',
+  :author => 'Dorothea Benton Frank',
+  :image => File.new(File.join(Rails.root, 'dummy_cover', '0062873520_b.jpg'))
+})
+Book.create({
+  :id => 3,
+  :title => 'War Storm',
+  :synopsis => 'But no battle is won alone, and before the Reds may rise as one, Mare must side with the boy who broke her heart in order to defeat the boy who almost broke her. Cal\'s powerful Silver allies, alongside Mare and the Scarlet Guard, prove a formidable force. But Maven is driven by an obsession so deep, he will stop at nothing to have Mare as his own again, even if it means demolish everything--and everyone--in his path.',
+  :author => 'Victoria Aveyard',
+  :image => File.new(File.join(Rails.root, 'dummy_cover', '0062422995_b.jpg'))
+})
+Book.create({
+  :id => 4,
+  :title => 'Three Days in Moscow : Ronald Reagan and the Fall of the Soviet Empire',
+  :synopsis => 'On May 31, 1988, Reagan addressed a packed audience at Moscow State University with an extraordinary speech that capped his first visit to the Soviet capital. This fourth in a series of summits between Reagan and Soviet General Secretary Mikhail Gorbachev, was the climactic breakthrough in their joint nuclear disarmament agreements. But it was far more than that. For Reagan, it was an opportunity to light a path for the Soviet people--toward freedom, human rights, and a future he told them they could embrace if they chose. It was the first time an American president gave a speech about human rights on Russian soil. Reagan had once called the Soviet Union an "evil empire." Now, saying that depiction was from "another time," he beckoned the Soviets to join him in a new vision. The success of the Moscow summit, which Reagan called "a grand historical moment," was captured in the eager faces of university students listening to the president of the United States deliver his transformational speech about the promise that awaited them. The importance of Reagan\'s Moscow speech was largely overlooked at the time, but the new world he spoke of was fast approaching; the following year, in November 1989, the Berlin Wall fell and the Soviet Union began to disintegrate, leaving the United States the sole superpower on the world stage. ',
+  :author => 'Catherine Whitney',
+  :image => File.new(File.join(Rails.root, 'dummy_cover', '006274836X_b.jpg'))
+})
+Book.create({
+  :id => 5,
+  :title => 'A Shout in the Ruins',
+  :synopsis => 'Seamlessly interwoven is the story of George Seldom, a man orphaned by the storm of the Civil War, looking back from the 1950s on the void where his childhood ought to have been. Watching the government destroy his neighborhood to build a stretch of interstate highway through Richmond, he travels south in an attempt to recover his true origins. With the help of a young woman named Lottie, he goes in search of the place he once called home, all the while reckoning with the more than 90 years he lived as witness to so much that changed during the 20th century, and so much that didn\'t. As we then watch Lottie grapple with life\'s disappointments and joys in the 1980\'s, now in her own middle-age, the questions remain: How do we live in a world built on the suffering of others? And can love exist in a place where for 400 years violence has been the strongest form of intimacy?',
+  :author => 'Kevin Powers',
+  :image => File.new(File.join(Rails.root, 'dummy_cover', '0316556475_b.jpg'))
+})
