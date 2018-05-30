14f25589ac435369310e70bfabe010cf4aabdd73
Paskahlis Anjas <pjaswo1204@gmail.com>
2018-05-30 15:17:23 +0700

    add method to retrieve the books borrowed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,31 +1,30 @@
   class BooksController < ApplicationController
   def show
     begin
-      @books = Book.find(params[:id])
-      render json: @books.jsonify, status: :ok
+      books = Book.find(params[:id])
+      render json: books.jsonify, status: :ok
     rescue ActiveRecord::RecordNotFound
       render json: { message: 'Could not find the book with id = ' + params[:id] }, status: 404
     end
   end
 
   def showall
-    @books = []
+    books = []
     Book.all.each do |book|
-      @books << book.jsonify
+      books << book.jsonify
     end
-    render json: @books, status: :ok
+    render json: books, status: :ok
   end
 
   def showseveral
-    @books = []
+    books = []
     Book.find_each(start: params[:start], finish: params[:finish]) do |book|
-      @books << book.jsonify
+      books << book.jsonify
     end
-    render json: @books, status: :ok
+    render json: books, status: :ok
   end
 
   def borrow
-
     auth_result = authorize
     unless auth_result[:authorized]
       render :json => { :message => auth_result[:message] }, :status => :unauthorized
@@ -33,9 +32,9 @@
     end
 
     begin
-      @books = Book.find(params[:id])
-      @books.stock -= 1
-      if @books.stock < 0
+      books = Book.find(params[:id])
+      books.stock -= 1
+      if books.stock < 0
         render json: { message: 'The book is out of stock' }, status: :ok
         return
       end
@@ -44,15 +43,14 @@
           :book_id => params[:id],
           :expiry_date => 72.hours.from_now
       })
-      @books.save
-      render json: @books.jsonify, status: :ok
+      books.save
+      render json: books.jsonify, status: :ok
     rescue ActiveRecord::RecordNotFound
       render json: { message: 'Could not find the book with id = ' + params[:id] }, status: :ok
     end
   end
 
   def return
-
     auth_result = authorize
     unless auth_result[:authorized]
       render :json => { :message => auth_result[:message] }, :status => :unauthorized
@@ -70,6 +68,25 @@
       end
       books.save
       render json: books.jsonify, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: 'Could not find the record' }, status: 404
+    end
+  end
+
+  def borrowed
+    auth_result = authorize
+    unless auth_result[:authorized]
+      render :json => { :message => auth_result[:message] }, :status => :unauthorized
+      return
+    end
+
+    begin
+      books = []
+      Borrow.where(user_id=auth_result[:user_id].to_s).find_each do |borrow|
+        puts borrow
+        books << Book.find(borrow.book_id)
+      end
+      render :json => books, :status => :ok
     rescue ActiveRecord::RecordNotFound
       render json: { message: 'Could not find the record' }, status: 404
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'books/:id' => 'books#show'
   post 'books/' => 'books#showseveral'
   get 'books/' => 'books#showall'
+  get 'books/borrowed/' => 'books#borrowed'
+  get 'books/:id' => 'books#show'
   get 'books/:id/borrow' => 'books#borrow'
   get 'books/:id/return' => 'books#return'
 
